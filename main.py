16bf3d747111eeebcf13a533304113afe4109300
dlgus2383 <dlgus2383@gmail.com>
2021-09-24 16:56:41 +0900

    ㅁㄴㅇ

--- main.py
+++ main.py
@@ -159,9 +159,7 @@ def Trend_30min():
 
 try:
     while True:
-        if((int(upbit.get_balance(Coinname)))==0):
-            print("1번 성공")
-        elif(int(upbit.get_balance(Coinname))==0):
+        if(int(upbit.get_balance(Coinname))==0):
             print("2번 성공")        
         elif(int(upbit.get_balance(Coinname))>0):
             print("3번 성공")
